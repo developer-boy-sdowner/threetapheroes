4d1f53ec969e800a75370681bdedda05956bd55b
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-07-09 16:13:13 +0530

    Update _token.js

--- _token.js
+++ _token.js
@@ -1,9 +1,9 @@
 module.exports = {
     //Put your bot related informations here
     APP_NAME : "MyPrettyBot",
-    BOT_TOKEN : "MyAwesomeToken",
+    BOT_TOKEN : "1523327279:AAGHN50ucG7nwDjX8b5KJwSwxpg-gdSIuIE",
     WEBHOOK : 'https://myawesomeurl.whatever', //https://telegram-bot-sdk.readme.io/docs/setwebhook
-    GAME_URL : 'https://mycoolgame.gg',
+    GAME_URL : 'https://games.cdn.famobi.com/html5games/t/train-2048/v150/?fg_domain=play.famobi.com&fg_aid=A1000-1&fg_uid=abf56d7c-b1c7-493b-b2ac-aecb45d00ef8&fg_pid=4638e320-4444-4514-81c4-d80a8c662371&fg_beat=473&original_ref=https%3A%2F%2Fhtml5games.com%2F',
     GAME_NAME : 'mycoolgame',
     PORT : 3000,    // Put your specific port here
     DEFAULT_PORT : 3000
